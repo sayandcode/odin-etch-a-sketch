0a1142d1356d2e331209b1a883e005269c9778ee
sayandcode <sayandcode@gmail.com>
2022-04-09 12:50:48 +0530

    Add prompt for new grid size

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
 const grid= document.querySelector('.grid');
-for (let i = 0; i < 16*16; i++) {
-    let sqr=document.createElement('div');
-    grid.appendChild(sqr);    
-}
-
-const sqrs=document.querySelectorAll('.grid > div');
-sqrs.forEach(sqr=>sqr.addEventListener('mouseover', e=>{
-    e.target.classList.add('etched');
-}))
+createNewGrid(16);
 
 const clrScrn=document.querySelector('.clrScrn');
 clrScrn.addEventListener('click',reset);
 
 function reset(){
-    sqrs.forEach(sqr=>sqr.classList.remove('etched'));
+    let s=prompt("Enter number of squares");
+    createNewGrid(s);
+}
+
+function createNewGrid(s){
+    //remove existing grid
+    let sqrs=document.querySelectorAll('.grid > div');
+    sqrs.forEach(sqr=> sqr.remove()); 
+
+    //create new grid
+    grid.style.gridTemplateColumns=`repeat(${s},${500/s}px)`;
+    grid.style.gridTemplateRows=`repeat(${s},${500/s}px)`;
+
+    for (let i = 0; i < s**2; i++) {
+        let sqr=document.createElement('div');
+        grid.appendChild(sqr);    
+    }
+
+    //and listen
+    sqrs=document.querySelectorAll('.grid > div');
+    sqrs.forEach(sqr=>sqr.addEventListener('mouseover', e=>{
+        e.target.classList.add('etched');
+    }))
 }
