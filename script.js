f0add7c03d2c1da1896b49f5402284467dd61d17
sayandcode <sayandcode@gmail.com>
2022-04-09 15:13:55 +0530

    Add color memory
    The color gets darker each time you run over it. Remaining code has been
    refactored to include the same functionality

--- script.js
+++ script.js
@@ -15,7 +15,11 @@ function createNewGrid(s){
     let sqrs=document.querySelectorAll('.grid > div');
     
     if(s===null){
-        sqrs.forEach(sqr=> sqr.classList.remove('etched'))
+        sqrs.forEach(sqr=> {
+            sqr.classList.remove('etched');
+            sqr.removeAttribute('style');
+        })
+
         return;
     }
 
@@ -34,7 +38,76 @@ function createNewGrid(s){
 
     //and listen
     sqrs=document.querySelectorAll('.grid > div');
-    sqrs.forEach(sqr=>sqr.addEventListener('mouseover', e=>{
-        e.target.classList.add('etched');
-    }))
+    sqrs.forEach(sqr=>sqr.addEventListener('mouseover',changecolor));
+}
+
+function changecolor(){
+    if(this.classList.contains('etched')){
+        let rgb=this.style.backgroundColor;
+        let hsl=RGB2HSL(rgb);
+        hsl[2]-=10;
+        this.style.backgroundColor=`hsl(${hsl[0]}, 100%, ${hsl[2]}%)`;
+    }
+    else{
+        this.classList.add('etched');
+        let hue=Math.floor(Math.random()*360);
+        this.style.backgroundColor=`hsl(${hue}, 100%, 50%)`;
+    }        
+}
+
+//https://css-tricks.com/converting-color-spaces-in-javascript/
+function RGB2HSL(rgb){
+    let sep = rgb.indexOf(",") > -1 ? "," : " ";
+    rgb = rgb.substr(4).split(")")[0].split(sep);
+
+    for (let R in rgb) {
+        let r = rgb[R];
+        if (r.indexOf("%") > -1) 
+        rgb[R] = Math.round(r.substr(0,r.length - 1) / 100 * 255);
+    }
+
+    // Make r, g, and b fractions of 1
+    let r = rgb[0] / 255,
+        g = rgb[1] / 255,
+        b = rgb[2] / 255;
+
+    // Find greatest and smallest channel values
+    let cmin = Math.min(r,g,b),
+    cmax = Math.max(r,g,b),
+    delta = cmax - cmin,
+    h = 0,
+    s = 0,
+    l = 0;
+
+    // Calculate hue
+    // No difference
+    if (delta == 0)
+    h = 0;
+    // Red is max
+    else if (cmax == r)
+    h = ((g - b) / delta) % 6;
+    // Green is max
+    else if (cmax == g)
+    h = (b - r) / delta + 2;
+    // Blue is max
+    else
+    h = (r - g) / delta + 4;
+
+    h = Math.round(h * 60);
+    
+    // Make negative hues positive behind 360°
+    if (h < 0)
+        h += 360;
+
+    // Calculate lightness
+    l = (cmax + cmin) / 2;
+
+    // Calculate saturation
+    s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+    
+    // Multiply l and s by 100
+    s = +(s * 100).toFixed(1);
+    l = +(l * 100).toFixed(1);
+
+    return [h,s,l];
 }
